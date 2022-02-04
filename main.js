2b47e016ca9d1a068a82111c5fc3b91cb697c28a
ChristianNikodemus <christian.nikodemus@gmail.com>
2022-02-03 22:07:43 -0500

    Implemented the switchMode function to change the type of timer for the app

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Sets time for different pomodoro buttons
 const timer = {
   pomodoro: 25,
   shortBreak: 5,
@@ -5,6 +6,7 @@ const timer = {
   longBreakInterval: 4,
 };
 
+// Adds functionality to user clicks for the mode buttons
 const modeButtons = document.querySelector("#js-mode-buttons");
 modeButtons.addEventListener("click", handleMode);
 
@@ -14,4 +16,22 @@ function handleMode(event) {
   if (!mode) return;
 
   switchMode(mode);
+}
+
+// The switchMode function sets a property to mode (longbreak, pomodoro, etc), and also remaining time on the timer
+function switchMode(mode) {
+  timer.mode = mode;
+  timer.remainingTime = {
+    total: timer[mode] * 60,
+    minutes: timer[mode],
+    seconds: 0,
+  };
+
+  document
+    .querySelectorAll("button[data-mode]")
+    .forEach((e) => e.classList.remove("active"));
+  document.querySelector(`[data-mode="${mode}"]`).classList.add("active");
+  document.body.style.backgroundColor = `var(--${mode})`;
+
+  updateClock();
 }
