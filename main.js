f4ae001b66232cd622d4824b78b1fe1f624327b1
ChristianNikodemus <christian.nikodemus@gmail.com>
2022-02-03 22:38:09 -0500

    Added class actions to timer

--- main.js
+++ main.js
@@ -68,6 +68,10 @@ function startTimer() {
   let { total } = timer.remainingTime;
   const endTime = Date.parse(new Date()) + total * 1000;
 
+  mainButton.dataset.action = "stop";
+  mainButton.textContent = "stop";
+  mainButton.classList.add("active");
+
   interval = setInterval(function () {
     timer.remainingTime = getRemainingTime(endTime);
     updateClock();
